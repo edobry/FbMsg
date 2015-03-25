66b1478ecc6fed05f8ead6ba42bcf2fb7aac2af5
Eugene Dobry <eugene@dobry.me>
2015-03-25 00:48:07 -0400

    split off browsing parameters

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var util = require("util"),
 	request = require("request"),
+	config = require("./config.json")
 	creds = require("./creds.json"),
 	template = require("./request.json");
 
+
+
 var process = function(res) {
 	var data = JSON.parse(res.substring(9));
-
 	console.log(data.payload.actions.map(function(msg) {
 	  return {
 	    author: msg.author == "fbid:" + creds.messagerId ? creds.messagerName : creds.messageeName,
@@ -23,8 +25,8 @@ var headerString = Object.keys(template.headers).map(function(name) {
 template.formdata.__user = creds.messagerId;
 template.formdata.__dyn = creds.dyn;
 
-template.formdata["messages[user_ids][" + creds.messageeId + "][offset]"] = 161;
-template.formdata["messages[user_ids][" + creds.messageeId + "][limit]"] = 80;
+template.formdata["messages[user_ids][" + creds.messageeId + "][offset]"] = config.offset;
+template.formdata["messages[user_ids][" + creds.messageeId + "][limit]"] = config.window;
 var formdataString = Object.keys(template.formdata)
 	.map(function(key) {
 		return util.format("%s=%s", key, template.formdata[key]); 
